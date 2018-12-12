5550d624c77ee6b666cc07739a3babb855cc2401
Emmanuel Madi <42469602+MadiEmmanuel@users.noreply.github.com>
2018-12-12 13:49:34 +0200

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,23 +1,28 @@
 
-    def fact(n):
+def fact(n):
     f=1
-    for i in range(1, n+1):
-        if num  >= 1:
+    if num  >= 1:
+        for i in range(1, n+1):
             f=f*i
-
-        elif   num < 1:
+            
+    elif   num < 1:
             print("Enter positive intergers only")
 
-        else:
-            print("Enter numeric numbers only")      
-        
+    else:
+            print("Enter numeric numbers only") 
+              
     return f    
-           
+        
+    
+    
 num = int(input("Enter number:"))
 
 result = fact(num)
        
 print(result)
+
+import unittest
+from factorial import fact
     
     
    
