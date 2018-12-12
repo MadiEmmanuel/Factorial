129097a146669ed47e6a281e5fc0b99f38cfb7bc
Emmanuel Madi <42469602+MadiEmmanuel@users.noreply.github.com>
2018-12-12 10:20:30 +0200

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,98 +1,20 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "num = int(input(\"Enter number:\"))\n",
-    "\n",
-    "def Fact(num):\n",
-    "    count = 0\n",
-    "    result = 1\n",
-    "    \n",
-    "    while (count >= 0):\n",
-    "        result = result * count\n",
-    "        \n",
-    "        count -= 1\n",
-    "        \n",
-    "    return print(result)\n",
-    "\n",
-    "Fact(num)\n",
-    "    \n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "\"\"\"for i in list:\n",
-    "        \n",
-    "        if 1*1/2 ==0:\n",
-    "            even += 1\n",
-    "        else:\n",
-    "            odd += 1\n",
-    "            \n",
-    "        return even, odd\n",
-    "    \n",
-    "\n",
-    "even, odd = count()\"\"\"\n",
-    "\n",
-    "\n",
-    "\n",
-    "\"\"\"1\n",
-    "2\n",
-    "3\n",
-    "4\n",
-    "5\n",
-    "6\n",
-    "7\n",
-    "8\n",
-    "9\n",
-    "10\n",
-    "num = int(input(\"enter a number: \"))\n",
-    " \n",
-    "fac = 1\n",
-    "i = 1\n",
-    " \n",
-    "while i <= num:\n",
-    "    fac = fac * i\n",
-    "    i = i + 1\n",
-    " \n",
-    "print(\"factorial of \", num, \" is \", fac)\"\"\"  \n",
-    "    \n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+
+    num = int(input("Enter number:)
+  
+    def Fact(num):
+        count = 0
+        result = 1
+      
+        while (count >= 0):
+            result = result * count
+           
+            count -= 1
+          
+        return print(result)
+    
+    Fact(num)
+    
+    
+   
+   
+  
